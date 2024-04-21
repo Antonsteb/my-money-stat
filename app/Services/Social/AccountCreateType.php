69f879bf51d2b864bbf65a641e8c588872d0f64b
Anton S <anton@mail.ru>
2024-04-21 09:47:44 +0300

    правка enum для входа по паролю

--- app/Services/Social/AccountCreateType.php
+++ app/Services/Social/AccountCreateType.php
@@ -4,19 +4,18 @@ namespace App\Services\Social;
 
 use App\Services\Social\Providers\DefaultUrlCreator;
 use App\Services\Social\Providers\Interfaces\RedirectUrlCreator;
-use App\Services\Social\Providers\SocialProvider;
 use App\Services\Social\Providers\VkSocialProvider;
 use App\Services\Social\Providers\YandexSocialProvider;
 
 enum AccountCreateType: string
 {
-    const Pass = 'pass';
+    case Pass = 'pass';
     case Vk = 'vk';
     case Yandex = 'yandex';
 
-    function getRedirectUrlCreator():RedirectUrlCreator
+    function getRedirectUrlCreator(): RedirectUrlCreator
     {
-        return match ($this){
+        return match ($this) {
             AccountCreateType::Vk => new VkSocialProvider(),
             AccountCreateType::Yandex => new YandexSocialProvider(),
             AccountCreateType::Pass => new DefaultUrlCreator(),
